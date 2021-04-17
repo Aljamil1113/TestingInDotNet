5bdb8d4436548f490bd7e2b8be1de2fbc8d3eab2
Aljamil1113 <aarazasaljamil@gmail.com>
2021-04-17 16:38:35 +0800

    Setup attr., paramameterized and Ignore sample test

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -10,47 +10,65 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        //Setup
+        //Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
+        [Ignore("I'm warning you!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-;
-            var result = math.Add(1, 2);
+           
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
 
         #region BLACKBOX
-        [Test]
-        public void Max_FirstArgumentsGreater_ReturnTheFirstArgument()
-        {
-            var math = new Math();
+        //[Test]
+        //public void Max_FirstArgumentsGreater_ReturnTheFirstArgument()
+        //{
 
-            var result = math.Max(2, 1);
+        //    var result = _math.Max(2, 1);
 
-            Assert.That(result, Is.EqualTo(2));
-        }
+        //    Assert.That(result, Is.EqualTo(2));
+        //}
 
-        [Test]
-        public void Max_SecondArgumentsGreater_ReturnTheSecondArgument()
-        {
-            var math = new Math();
+        //[Test]
+        //public void Max_SecondArgumentsGreater_ReturnTheSecondArgument()
+        //{
+        //    var result = _math.Max(2, 3);
 
-            var result = math.Max(2, 3);
-
-            Assert.That(result, Is.EqualTo(3));
-        }
+        //    Assert.That(result, Is.EqualTo(3));
+        //}
 
 
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            var math = new Math();
+        //[Test]
+        //public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        //{
+        //    var result = _math.Max(1, 1);
 
-            var result = math.Max(1, 1);
-
-            Assert.That(result, Is.EqualTo(1));
-        }
+        //    Assert.That(result, Is.EqualTo(1));
+        //}
         #endregion
+
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(2,3,3)]
+        [TestCase(1,1,1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
