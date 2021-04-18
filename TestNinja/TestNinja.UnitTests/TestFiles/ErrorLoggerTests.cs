8b0df879e231cbbc7555362c7eceadafceca270f
Aljamil1113 <aarazasaljamil@gmail.com>
2021-04-18 20:19:42 +0800

    Testing Method that Throw Exceptions and Test Events

--- TestNinja/TestNinja.UnitTests/TestFiles/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/TestFiles/ErrorLoggerTests.cs
@@ -20,5 +20,29 @@ namespace TestNinja.UnitTests.TestFiles
 
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
